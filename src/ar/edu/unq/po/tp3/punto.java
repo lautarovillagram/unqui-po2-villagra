1c0a7e24be44e80c2ae98df05cfa6528cf04a122
Lauta <Lauta@DESKTOP-BQ3238V>
2022-04-07 15:35:27 -0300

    creacion clase punto

---
+++ src/ar/edu/unq/po/tp3/punto.java
@@ -0,0 +1,26 @@
+package ar.edu.unq.po.tp3;
+
+public class punto {
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public punto(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	private float x;
+	private float y;
+	
+	
+
+}
